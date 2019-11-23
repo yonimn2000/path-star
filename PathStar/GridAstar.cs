ad5fc39ec827bc854928da4fc540f892fc6aacc7
Yonatan Mankovich <yonimn2000@gmail.com>
2019-11-23 11:09:00 -0500

    Bug fixes.

--- PathStar/GridAstar.cs
+++ PathStar/GridAstar.cs
@@ -10,19 +10,17 @@ namespace YonatanMankovich.PathStar
         public Point StartPoint { get; }
         public Point EndPoint { get; }
         public bool IsDone { get; private set; }
+        public List<Point> Path { get; private set; } = new List<Point>();
 
         private GridPoint[,] Grid { get; set; }
-        private List<GridPoint> Path { get; set; } = new List<GridPoint>();
-        private List<GridPoint> OpenSet { get; set; } = new List<GridPoint>();
-        private List<GridPoint> ClosedSet { get; set; } = new List<GridPoint>();
+        public List<GridPoint> OpenSet { get; private set; } = new List<GridPoint>();
+        public List<GridPoint> ClosedSet { get; private set; } = new List<GridPoint>();
 
         public GridAstar(Size gridSize, Point startPoint, Point endPoint, List<Point> wallPoints)
         {
             GridSize = gridSize;
             StartPoint = startPoint;
             EndPoint = endPoint;
-
-            OpenSet.Add(new GridPoint(StartPoint));
             Grid = new GridPoint[GridSize.Height, GridSize.Width];
 
             for (int y = 0; y < GridSize.Height; y++)
@@ -35,21 +33,34 @@ namespace YonatanMankovich.PathStar
 
             foreach (Point wallPoint in wallPoints)
                 Grid[wallPoint.Y, wallPoint.X].IsWall = true;
+
+            Grid[StartPoint.Y, StartPoint.X].IsWall = false;
+            Grid[EndPoint.Y, EndPoint.X].IsWall = false;
+            OpenSet.Add(Grid[StartPoint.Y, StartPoint.X]);
         }
 
         public void FindPath()
         {
-            while (OpenSet.Count > 0 || !IsDone)
+            while (HasNextStep())
                 MakeStep();
             IsDone = true;
             //if (OpenSet.Count == 0) return "No solution"
+        }
+
+        public bool HasNextStep()
+        {
+            return OpenSet.Count > 0 && !IsDone;
         }
 
         public void MakeStep()
         {
             GridPoint currentPoint = GetCurrentPoint();
             if (currentPoint.X == EndPoint.X && currentPoint.Y == EndPoint.Y)
+            {
                 IsDone = true;
+                BuildPathToStartFromCurrentPoint(currentPoint);
+                return;
+            }
             OpenSet.Remove(currentPoint);
             ClosedSet.Add(currentPoint);
             foreach (GridPoint neighbor in currentPoint.Neighbors)
@@ -79,12 +90,17 @@ namespace YonatanMankovich.PathStar
                     }
                 }
             }
+            BuildPathToStartFromCurrentPoint(currentPoint);
+        }
 
+        private void BuildPathToStartFromCurrentPoint(GridPoint currentPoint)
+        {
             GridPoint walker = currentPoint;
-            Path.Add(walker);
+            Path.Clear();
+            Path.Add(walker.GetAsPoint());
             while (walker.Previous != null)
             {
-                Path.Add(walker.Previous);
+                Path.Add(walker.Previous.GetAsPoint());
                 walker = walker.Previous;
             }
         }
@@ -104,7 +120,7 @@ namespace YonatanMankovich.PathStar
         }
     }
 
-    internal class GridPoint
+    public class GridPoint
     {
         public int X { get; set; }
         public int Y { get; set; }
@@ -137,6 +153,11 @@ namespace YonatanMankovich.PathStar
                 Neighbors.Add(grid[Y, X + 1]);
             if (Y < grid.GetLength(0) - 1) // If not the last row
                 Neighbors.Add(grid[Y + 1, X]);
+        }
+
+        public Point GetAsPoint()
+        {
+            return new Point(X, Y);
         }
     }
 }
