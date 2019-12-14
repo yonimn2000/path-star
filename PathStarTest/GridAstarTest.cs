d96bcd40ec3afa5ff089440c40895343d90a47b0
Yonatan Mankovich <yonimn2000@gmail.com>
2019-12-14 17:55:15 -0500

    Split tests.

---
+++ PathStarTest/GridAstarTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using YonatanMankovich.PathStar;
+
+namespace YonatanMankovich.PathStarTest
+{
+    static class GridAstarTest
+    {
+        public static void Test(GridAstar gridAstar, List<Point> wallPoints)
+        {
+            while (gridAstar.HasNextStep())
+            {
+                gridAstar.MakeStep();
+                if (gridAstar.OpenSet.Count == 0)
+                {
+                    Console.SetCursorPosition(0, gridAstar.GridSize.Height);
+                    Console.Write("No path...");
+                    break;
+                }
+                Console.CursorVisible = false;
+
+                foreach (Point wallPoint in wallPoints)
+                    Helper.DrawPointInColor(wallPoint, ConsoleColor.Gray);
+
+                foreach (GridPoint closedSetPoint in gridAstar.ClosedSet)
+                    Helper.DrawPointInColor(closedSetPoint.GetAsPoint(), ConsoleColor.Cyan);
+
+                foreach (GridPoint openSetPoint in gridAstar.OpenSet)
+                    Helper.DrawPointInColor(openSetPoint.GetAsPoint(), ConsoleColor.Green);
+
+                foreach (Point pathPoint in gridAstar.Path)
+                    Helper.DrawPointInColor(pathPoint, ConsoleColor.Yellow);
+
+                Thread.Sleep(100);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- PathStarTest/Program.cs
+++ PathStarTest/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Threading;
 using YonatanMankovich.PathStar;
 
 namespace YonatanMankovich.PathStarTest
@@ -22,42 +21,7 @@ namespace YonatanMankovich.PathStarTest
                 wallPoints.Add(new Point(random.Next(gridSize.Width), random.Next(gridSize.Height)));
 
             gridAstar = new GridAstar(gridSize, startPoint, endPoint, wallPoints);
-
-            while (gridAstar.HasNextStep())
-            {
-                gridAstar.MakeStep();
-                if (gridAstar.OpenSet.Count == 0)
-                {
-                    Console.SetCursorPosition(0, gridSize.Height);
-                    Console.Write("No path...");
-                    break;
-                }
-                Console.CursorVisible = false;
-
-                foreach (Point wallPoint in wallPoints)
-                    DrawPointInColor(wallPoint, ConsoleColor.Gray);
-
-                foreach (GridPoint closedSetPoint in gridAstar.ClosedSet)
-                    DrawPointInColor(closedSetPoint.GetAsPoint(), ConsoleColor.Cyan);
-
-                foreach (GridPoint openSetPoint in gridAstar.OpenSet)
-                    DrawPointInColor(openSetPoint.GetAsPoint(), ConsoleColor.Green);
-
-                foreach (Point pathPoint in gridAstar.Path)
-                    DrawPointInColor(pathPoint, ConsoleColor.Yellow);
-
-                Thread.Sleep(100);
-            }
-            Console.ReadLine();
-        }
-
-        private static void DrawPointInColor(Point point, ConsoleColor color)
-        {
-            ConsoleColor prevColor = Console.BackgroundColor;
-            Console.SetCursorPosition(point.X * 2, point.Y);
-            Console.BackgroundColor = color;
-            Console.Write("  ");
-            Console.BackgroundColor = prevColor;
+            GridAstarTest.Test(gridAstar, wallPoints);
         }
     }
 }
